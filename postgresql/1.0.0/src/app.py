cf58b9cca08be91de0cb9e0e3a6d700e93af7c48
Marius Urkis <marius@urkis.lt>
2024-02-15 12:13:26 +0200

    error handling fix

--- postgresql/1.0.0/src/app.py
+++ postgresql/1.0.0/src/app.py
@@ -12,7 +12,7 @@ from walkoff_app_sdk.app_base import AppBase
 
 class PostgreSQL(AppBase):
     __version__ = "1.0.0"
-    app_name = "PostgreSQL"  # this needs to match "name" in api.yaml
+    app_name = "PostgreSQL"
 
     def __init__(self, redis, logger, console_logger=None):
         """
@@ -23,22 +23,17 @@ class PostgreSQL(AppBase):
         """
         super().__init__(redis, logger, console_logger)
 
-    def connection(self, username, password, host, port, database_name):
-        try:
-            conn = connect(user=username, password= password,
-                                        host= host,
-                                        port=port,
-                                        database= database_name)
-            print(f"Connection successful, User -->{username} ")                                
-        except OperationalError as err:
-            print_psycopg2_exception(err)
-            conn = None
-        return conn  
-
-
     def query_postgresql_database(self, username, password, host, port, database_name, query):
         
-        self.db_connection = self.connection(username, password, host, port, database_name) 
+        try:
+            conn = connect(user=username, password= password,
+                            host= host,
+                            port=port,
+                            database= database_name)
+            print(f"Connection successful {username}@{database_name} ")                                
+        except OperationalError as err:
+            conn = None
+            return {"Error": f"psycopg2 connection ERROR: {err}, pgerror: {err.pgerror}, pgcode:{err.pgcode}"}
         if self.db_connection != None:
             cursor = self.db_connection.cursor()
             try:
@@ -49,7 +44,7 @@ class PostgreSQL(AppBase):
                 self.db_connection.close()
                 return (json.dumps(res))  
             except Exception as err:
-                print_psycopg2_exception(err)
+                return {"Error": f"psycopg2 query ERROR: {err}, pgerror: {err.pgerror}, pgcode:{err.pgcode}"}
 
 if __name__ == "__main__":
     PostgreSQL.run()
